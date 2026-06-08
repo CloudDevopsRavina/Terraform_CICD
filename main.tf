a02a68c4bb9e7f421799719d2140a4b92ab9c779
Ravina Rathod <ravinarathod009@gmail.com>
2026-06-08 07:55:40 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 provider "aws" {
-  region = "us-east-1"
+  region = "ap-south-1"
 }
 
 resource "aws_instance" "dev" {
-    ami = "ami-0f88e80871fd81e91"
-    instance_type = "t.micro"
+    ami = "ami-0685bcc683dadb6b9"
+    instance_type = "t2.micro"
     tags = {
-      Name = "ravi"
+      Name = "ravina"
     }
 }
